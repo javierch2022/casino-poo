32d64aef5763946ca2238682f3d8c573f2ba0fd1
Emiliano Gonzalez <emilianogonzalez@tdf.edu.ar>
2022-10-25 20:15:36 -0300

    juego.ts y tragamonedas.ts

--- class/Juego.ts
+++ class/Juego.ts
@@ -4,7 +4,42 @@ import { Tragamonedas } from "./juegos/tragamonedas";
 
 
 export class Juego {
+    protected nombre: string;
+    protected plata: number;
+    protected credito: number;
+    protected montoApostado: number;
 
+    constructor(pNombre: string, pPlata: number, pCredito: number, pMontoApostado: number) {
+        this.nombre = pNombre;
+        this.plata = pPlata;
+        this.credito = pCredito;
+        this.montoApostado = pMontoApostado;
+    }
+
+    public getNombre(): string {
+        return this.nombre;
+    }
+    public setNombre(pNombre: string): void {
+        this.nombre = pNombre;
+    }
+    public getPlata(): number {
+        return this.plata;
+    }
+    public setPlata(pPlata: number): void {
+        this.plata = pPlata;
+    }
+    public getCredito(): number {
+        return this.credito;
+    }
+    public setCredito(pCredito: number): void {
+        this.plata = pCredito;
+    }
+    public getMontoApostado(): number {
+        return this.montoApostado;
+    }
+    public setMontoApostado(pMontoApostado: number): void {
+        this.montoApostado = pMontoApostado;
+    }
 }
 
 
--- class/juegos/tragamonedas.ts
+++ class/juegos/tragamonedas.ts
@@ -1,4 +1,43 @@
+import { Juego } from "../Juego";
+export class Tragamonedas extends Juego{
+    protected cantidadColumna : number;
+    protected cantidadFigura : number;
+    protected palanca : boolean;
+    protected pagoCasa : number;
 
-export class Tragamonedas{
+    constructor(pNombre:string,pPlata:number,pCredito:number,pMontoApostado:number,pCantColum:number,pCantFig:number,pPalanca:boolean,pPagoCasa:number){
+        super(pNombre,pPlata,pCredito,pMontoApostado)
+        this.cantidadColumna = pCantColum;
+        this.cantidadFigura = pCantFig;
+        this.palanca = pPalanca;
+        this.pagoCasa = pPagoCasa;
+    }
 
-};
+    public getCantColum(): number {
+        return this.cantidadColumna;
+    }
+    public setCantColum(pCantColum: number): void {
+        this.cantidadColumna = pCantColum;
+    }
+    public getCantFig(): number {
+        return this.cantidadFigura;
+    }
+    public setCantFig(pCantFig: number): void {
+        this.cantidadFigura = pCantFig;
+    }
+    public getPagoCasa(): number {
+        return this.pagoCasa;
+    }
+    public setPagoCasa(pPagoCasa: number): void {
+        this.pagoCasa = pPagoCasa;
+    }
+    public ejecPalanca(palanca:boolean): void{
+        this.palanca=palanca;
+        if(palanca==true){
+            /*definir accion - puede jugar? ejecutar juego?*/
+        } else {
+            /*definir accion - no puede jugar por falta de credito*/
+        }
+    }
+
+}
