0781e4aeae9729aa80122c8b591cf1594d6f2eb0
Emiliano Gonzalez <emilianogonzalez@tdf.edu.ar>
2022-10-25 21:02:31 -0300

    se definio ruleta y blackjack

--- class/juegos/blackJack.ts
+++ class/juegos/blackJack.ts
@@ -1,4 +1,33 @@
+import { Juego } from "../Juego";
+export class BlackJack/*extends Juego*/{
+    protected cantidadCartas:number;
+    /*protected listaCartas:carta(numero,figura)[]; <--- no se definio carta*/
+    protected cantidadApuesta:number;
+    protected resultado:string;
 
-export class BlackJack{
-    
+    constructor(/*pNombre:string,pPlata:number,pCredito:number,pMontoApostado:number,*/pCantCart:number,/*pListaCartas:carta[],*/pCantApuesta:number,pResultado:string){
+        /*super(pNombre,pPlata,pCredito,pMontoApostado)*/
+        this.cantidadCartas = pCantCart;
+        this.cantidadApuesta = pCantApuesta;
+        /*this.listaCartas[] = pListaCartas[]*/
+        this.resultado = pResultado;
+    };
+    public getCantCart(): number {
+        return this.cantidadCartas;
+    }
+    public setCantCart(pCantCart: number): void {
+        this.cantidadCartas = pCantCart;
+    }
+    public getCantidadApuesta(): number {
+        return this.cantidadApuesta;
+    }
+    public setCantApuesta(pCantApuesta: number): void {
+        this.cantidadApuesta = pCantApuesta;
+    }
+    public getResultado(): string {
+        return this.resultado;
+    }
+    public setResultado(pResultado: string): void {
+        this.resultado = pResultado;
+    }
 }
--- class/juegos/ruleta.ts
+++ class/juegos/ruleta.ts
@@ -1,4 +1,26 @@
+import { Juego } from "../Juego";
+export class Ruleta /*extends Juego*/{
+    protected numeroTablero:number[];
+    protected listaNumerosRojos:number[];
+    protected listaNumerosNegros:number[];
+    protected listaPrimeraDecena:number[];
+    protected listaSegundaDecena:number[];
+    protected listaTerceraDecena:number[];
+    protected listaPrimeraColumna :number[];
+    protected listaSegundaColumna :number[];
+    protected listaTercerColumna :number[];
+    protected lista1a18 :number[];
+    protected lista19a36 :number[];
+    protected listaPar :number[];
+    protected listaImpan :number[];
+    //*protected ubicacionApuesta:Apuesta(ubicacion,monto)[];*//  <--- nunca definimos "apuesta"
+    protected pagoCasa : number;
 
-export class Ruleta{
+    constructor(/*pNombre:string,pPlata:number,pCredito:number,pMontoApostado:number,*/pNumTab:number,pListNumRoj:number[],pListNumNeg:number[],
+    pListPrimDec:number[],pListSegDec:number[],pListTercDec:number[],pListPrimCol:number[],pListSegCol:number[],pListTerCol:number[],
+    pList1a18:number[],pList19a36:number[],pListPar:number[],pListImpar:number[],pPagoCasa:number){
+        /*super(pNombre,pPlata,pCredito,pMontoApostado)*/
+    };
     
+
 }
