d4202af156f117c11c5f518618cc1fff79c53b43
Emiliano Gonzalez <emilianogonzalez@tdf.edu.ar>
2022-10-27 20:54:29 -0300

    se inicio codigo blackjack

--- class/juegos/blackJack.ts
+++ class/juegos/blackJack.ts
@@ -1,34 +1,60 @@
 import { Juego } from "../Juego";
-
-export class BlackJack extends Juego{
-    protected cantidadCartas:number;
-    protected listaCartas:carta(numero,figura)[]; 
-    protected cantidadApuesta:number;
-    protected resultado:string;
-
-    constructor(pNombre:string,pPlata:number,pCredito:number,pMontoApostado:number,pCantCart:number,pListaCartas:carta[],pCantApuesta:number,pResultado:string){
-        super(pNombre,pCredito,pMontoApostado)
-        this.cantidadCartas = pCantCart;
-        this.cantidadApuesta = pCantApuesta;
-        this.listaCartas[] = pListaCartas[]
-        this.resultado = pResultado;
-    };
-    public getCantCart(): number {
-        return this.cantidadCartas;
-    }
-    public setCantCart(pCantCart: number): void {
-        this.cantidadCartas = pCantCart;
-    }
-    public getCantidadApuesta(): number {
-        return this.cantidadApuesta;
-    }
-    public setCantApuesta(pCantApuesta: number): void {
-        this.cantidadApuesta = pCantApuesta;
-    }
-    public getResultado(): string {
-        return this.resultado;
-    }
-    public setResultado(pResultado: string): void {
-        this.resultado = pResultado;
+class ApuestaBlackJack {
+    private creditoApuesta: number;
+    constructor(creditoApuesta: number) {
+        this.creditoApuesta = creditoApuesta;
     }
 }
+
+export class BlackJack extends Juego {
+    protected totalCartas: number; // todas las cartas
+    protected mazoCartas: string[]; // sumar valores de cart
+    protected numeroCartas: any[]; // cartas de 1 a 10
+    protected palos: string[]; //corazon,diamante,etc
+    protected cartasEspeciales: string[]; // j,q,k,a 
+    protected pedirCartas: boolean;
+    protected noMasCartas: boolean;
+    protected pagoCasa: number;
+   
+
+    constructor(pNombre: string, pCredito: number, pMontoApostado: number, pPedirCartas: boolean, pNoMasCartas: boolean, pPagoCasa: number) {
+        super(pNombre, pCredito, pMontoApostado);
+
+        this.totalCartas = 52;
+        this.numeroCartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,'J','Q','K','A'];
+        this.palos = ['C', 'D', 'P', 'T']; // C = CORZARON, D = DIAMANTE, P = PICA, T = TREBOL
+        this.cartasEspeciales = ['J', 'Q', 'K', 'A'];
+        this.pedirCartas = pPedirCartas;
+        this.noMasCartas = pNoMasCartas;
+        this.pagoCasa = pPagoCasa;
+    }
+
+    //--------get & set-------//
+    public setPedirCartas(): void {
+        this.pedirCartas = true;
+    }
+    public getPedirCartas(): boolean {
+        return this.pedirCartas;
+    }
+    public setNoMasCartas(): void {
+        this.noMasCartas = true;
+    }
+    public getNoMasCartas(): boolean {
+        return this.noMasCartas;
+    }
+    //--------metodos---------//
+    public validarCreditos() {
+        if (this.credito > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public pagarApuesta(pCredito: number): void {
+        this.credito += pCredito;
+    }
+    public cobrarApuesta(pCredito: number): void {
+        this.credito -= pCredito;
+    }
+}
+//----SE NOS QUEMARON LOS LIBROS----//
