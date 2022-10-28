43f6717113c7085f6984e1d444f02fd74418c273
Emiliano <emilianogonzalez@tdf.edu.ar>
2022-10-28 20:50:19 -0300

    se instalo read line

--- class/juegos/blackJack.ts
+++ class/juegos/blackJack.ts
@@ -9,24 +9,27 @@ class ApuestaBlackJack {
 export class BlackJack extends Juego {
     protected totalCartas: number; // todas las cartas
     protected mazoCartas: string[]; // sumar valores de cart
-    protected numeroCartas: any[]; // cartas de 1 a 10
-    protected palos: string[]; //corazon,diamante,etc
-    protected cartasEspeciales: string[]; // j,q,k,a 
+    protected numeroCartas: string[]; // cartas de 1 a 10
+    /*protected palos: string[]; //corazon,diamante,etc
+    protected cartasEspeciales: string[]; // j,q,k,a */
     protected pedirCartas: boolean;
     protected noMasCartas: boolean;
     protected pagoCasa: number;
+    protected cartasJugador: number;
+    protected cartasPC:number;
    
 
     constructor(pNombre: string, pCredito: number, pMontoApostado: number, pPedirCartas: boolean, pNoMasCartas: boolean, pPagoCasa: number) {
         super(pNombre, pCredito, pMontoApostado);
 
         this.totalCartas = 52;
-        this.numeroCartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,'J','Q','K','A'];
-        this.palos = ['C', 'D', 'P', 'T']; // C = CORZARON, D = DIAMANTE, P = PICA, T = TREBOL
-        this.cartasEspeciales = ['J', 'Q', 'K', 'A'];
+        this.numeroCartas = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+       /* this.palos = ['C', 'D', 'P', 'T']; // C = CORZARON, D = DIAMANTE, P = PICA, T = TREBOL
+        this.cartasEspeciales = ['J', 'Q', 'K', 'A'];*/
         this.pedirCartas = pPedirCartas;
         this.noMasCartas = pNoMasCartas;
         this.pagoCasa = pPagoCasa;
+
     }
 
     //--------get & set-------//
@@ -56,5 +59,14 @@ export class BlackJack extends Juego {
     public cobrarApuesta(pCredito: number): void {
         this.credito -= pCredito;
     }
+    public entregarCarta(): number {
+        return Math.round(Math.random() * (14 - 1) + 1);
+    }
+    public jugar():void{
+        let jugador : number = 0;
+        if(this.validarCreditos()){
+            while(this.pedirCartas)
+            jugador += this.entregarCarta(); //
+        }
+    }
 }
-//----SE NOS QUEMARON LOS LIBROS----//
